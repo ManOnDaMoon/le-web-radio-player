e05d979b27e3c8ca6f3c8ffb15f2482a65c40a22
ManOnDaMoon <manondamoon@gmail.com>
2026-02-12 14:25:32 +0100

    Still better low battery alerts

--- main-luma.py
+++ main-luma.py
@@ -299,7 +299,7 @@ class PiWebRadioApp():
                         if self.power_alert > 0:
                             old_volume = self.radio.volume
                             self.radio.set_volume(0)
-                            self.display_splash(os.path.join(self.__script_dir_name, "lowpower.bmp"), 5)
+                            self.display_splash(os.path.join(self.__script_dir_name, "lowpower.bmp"), 3)
                             self.power_alert = 0
                             self.radio.set_volume(old_volume)
                             continue
@@ -400,10 +400,7 @@ class PiWebRadioApp():
                     os.system("sudo shutdown -h now")
             else:
                 self.power_alert = 0
-            if self.power:
-                time.sleep(30)
-            else:
-                time.sleep(60)
+            time.sleep(60)
 
 
     # API ROUTES
