6f5c980dddadcd1e985d92df85d99111de11332c
ManOnDaMoon <manondamoon@gmail.com>
2026-03-24 10:17:42 +0100

    Change shutdown and reboot process

--- main-luma.py
+++ main-luma.py
@@ -175,7 +175,7 @@ class PiWebRadioApp():
         button.was_held = True
         self.shutdown_tasks()
         print(f"{time.ctime(time.time())} : Extinction totale par bouton physique")
-        os.system("sudo systemctl power off")
+        os.system("sudo systemctl poweroff")
         exit(0)
 
     def button_volume_up(self, rotary_encoder: RotaryEncoder):
@@ -399,7 +399,7 @@ class PiWebRadioApp():
                 print(f"{time.ctime(self.battery_alert_time)} : Alerte batterie faible {self.battery_percentage}")
                 if self.battery_percentage <= 7.0:
                     print(f"{time.ctime(self.battery_alert_time)} : Extinction batterie faible")
-                    os.system("sudo systemctl power off")
+                    os.system("sudo systemctl poweroff")
             else:
                 self.power_alert = 0
             time.sleep(60)
@@ -471,7 +471,7 @@ class PiWebRadioApp():
     def api_total_shutdown(self):
         self.shutdown_tasks()
         print(f"{time.ctime(time.time())} : Extinction totale par API")
-        os.system("sudo systemctl power off")
+        os.system("sudo systemctl poweroff")
         exit(0)
 
     def api_reboot(self):
